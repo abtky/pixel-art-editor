d715328bc2d2768cb4e799230240972e632f60bb
Takuya Abe <abe_takuya@cocone.co.jp>
2020-11-29 19:26:34 +0900

    feat: Install Prettier and format code.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,31 +5,35 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:@typescript-eslint/recommended",
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2020,
-    sourceType: "module",
+    sourceType: 'module',
   },
-  plugins: ["react", "@typescript-eslint"],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
       },
     },
+    sourceType: 'module',
   },
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    "@typescript-eslint/indent": ["error", 2],
-    "react/jsx-filename-extension": ["error", { extensions: [".jsx", ".tsx"] }],
-    "react/prop-types": "off",
-    "spaced-comment": ["error", "always", { markers: ["/ <reference"] }],
-    "import/extensions": "off",
+    '@typescript-eslint/indent': ['error', 2],
+    '@typescript-eslint/prefer-interface': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/prop-types': 'off',
+    'spaced-comment': ['error', 'always', { markers: ['/ <reference'] }],
+    'import/extensions': 'off',
   },
 };
