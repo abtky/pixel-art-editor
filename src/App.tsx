12b3c34f75f6151c8b75464a33b464304a94b3cc
Takuya Abe <abe_takuya@cocone.co.jp>
2020-11-29 23:28:46 +0900

    refactor: Remove unnecessary files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import io from 'socket.io-client';
-import logo from './logo.svg';
-import './App.css';
 
 const getSocket = (): SocketIOClient.Socket => {
   // const isDevelop: boolean = process.env.NODE_ENV === 'development';
@@ -12,29 +10,18 @@ const getSocket = (): SocketIOClient.Socket => {
 const socket = getSocket();
 socket.open();
 socket.emit('connection', 'message from client');
-console.log('connection', socket);
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
           <code>src/App.tsx</code>
           and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
-}
-
+};
 export default App;
