51efd23201c19803093126deb2b23ed3e4e75466
Takuya Abe <abe_takuya@cocone.co.jp>
2020-11-30 12:36:13 +0900

    add(client): Add web-font files.

--- src/style.ts
+++ src/style.ts
@@ -1,13 +1,32 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
+export const cssVars = {
+  fontFamily: '"4Mini"',
+  fontSize_S: '8px',
+  fontSize_M: '16px',
+  fontSize_L: '24px',
+};
+
 export const GlobalStyle = createGlobalStyle`
   ${reset}
+  @font-face {
+    font-family: ${cssVars.fontFamily};
+    font-weight: normal;
+    font-style: normal;
+
+    src: url('assets/font/4Mini.ttf.woff') format('woff'),
+    url('assets/font/4Mini.ttf.svg#4Mini') format('svg'), 
+    url('assets/font/4Mini.ttf.eot'),
+    url('assets/font/4Mini.ttf.eot?#iefix') format('embedded-opentype');
+  }
 
   body {
     margin: 0;
     background-color: #282c34;
     color: white;
+    font-family: ${cssVars.fontFamily};
+    font-size: ${cssVars.fontSize_S};
 
    -webkit-font-smoothing: antialiased;
    -moz-osx-font-smoothing: grayscale;
