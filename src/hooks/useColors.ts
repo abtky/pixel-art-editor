4661f84452cd650d4105a8eda07f888666c5d87c
Takuya Abe <abe_takuya@cocone.co.jp>
2020-12-06 20:53:57 +0900

    feat(client): Enables to choose a grayscale color

--- src/hooks/useColors.ts
+++ src/hooks/useColors.ts
@@ -3,9 +3,18 @@ export const useColors = (cols: number, rows: number): string[] => {
     const length = c * r;
     return new Array(length).fill('').map((val, i) => {
       const hue = Math.floor(((i % c) / c) * 360);
-      const lightness = 100 - Math.floor(i / c) * rows;
+      const lightness = 100 - Math.ceil((i + 1) / c) * rows;
       return `hsl(${hue}, 100%, ${lightness}%)`;
     });
   };
-  return createColors(cols, rows);
+  const createGrayScales = (c: number): string[] => {
+    return new Array(c).fill('').map((v, i) => {
+      const scale = 1 - i / c;
+      const value = Math.floor(scale * 0xff);
+      return `rgba(${value},${value},${value},1.0)`;
+    });
+  };
+  const colors = createColors(cols, rows);
+  const grayScales = createGrayScales(cols);
+  return grayScales.concat(colors);
 };
