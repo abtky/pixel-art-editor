1337653dc38e2206b1a5ce65322d260dbf353c01
Takuya Abe <abe_takuya@cocone.co.jp>
2020-12-07 18:49:19 +0900

    modify(server): Translate test comments.

--- __test__/Game.spec.ts
+++ __test__/Game.spec.ts
@@ -3,13 +3,17 @@ describe('Game', () => {
   const game = new Game();
   const result = game.toString();
 
-  it('Game#toString it will be get game information by JSON strings', () => {
-    expect(typeof result).toBe('string'); // result type is 'string'
-    expect(() => JSON.parse(result)).not.toThrow(); // and it is valid json.
+  it('Game#toString ゲーム情報をJSON文字列で取得', () => {
+    expect(() => JSON.parse(result)).not.toThrow();
   });
-  it('Game#toString that has keys "cols", "rows", "grids".', () => {
+  it('Game#toString ゲーム情報は"cols", "rows", "grids"のキーを持つ', () => {
     const keys = ['cols', 'rows', 'grids'];
     const parsedResult = JSON.parse(result);
     expect(Object.keys(parsedResult)).toEqual(keys);
+  });
+  it('Game#toString "grids"は「col」と「rows」を掛けた長さの配列', () => {
+    const parsedResult = JSON.parse(result);
+    const { grids, cols, rows } = parsedResult;
+    expect(grids.length).toBe(cols * rows);
   });
 });
