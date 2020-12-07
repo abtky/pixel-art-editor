f6dee9e9157f1bd9ea825f72f752a013ba4b3025
Takuya Abe <abe_takuya@cocone.co.jp>
2020-12-07 19:27:32 +0900

    feat(server): Implement User.toValidName function.

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,26 @@
 class User {
+  static get MAX_NAME_LENGTH() {
+    return 12;
+  }
+
+  static toValidName(value) {
+    const letters = value.split(''); // 文字列を配列に分割
+    const pattern = /[0-9a-z]/;
+    const filtered = letters.filter((letter) => {
+      return letter.match(pattern);
+    });
+    const result = filtered.slice(0, User.MAX_NAME_LENGTH).join('');
+    return result || 'unknown';
+  }
+
   constructor(id, name = 'unknown') {
     this.id = id;
-    this.name = name;
+    this.setName(name);
     this.color = 'white';
+  }
+
+  setName(value) {
+    this.name = User.toValidName(value);
   }
 
   toString() {
