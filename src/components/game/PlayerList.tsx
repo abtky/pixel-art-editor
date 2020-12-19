0b712940e05048f24efed75277a7305f1ef43717
Takuya Abe <abe_takuya@cocone.co.jp>
2020-12-20 02:48:27 +0900

    Modify: Adjust stylesheet.

--- src/components/game/PlayerList.tsx
+++ src/components/game/PlayerList.tsx
@@ -19,7 +19,7 @@ const PlayerList: React.FC<Props> = (props: Props) => {
             const isYourData = yourInfo.id === player.id;
             const you = isYourData ? ` (YOU)` : '';
             return (
-              <StyledListItem key={player.id}>
+              <StyledListItem key={player.id} emphasize={isYourData}>
                 <StyledGird color={player.color} />
                 {player.name}
                 {you}
@@ -34,16 +34,22 @@ const PlayerList: React.FC<Props> = (props: Props) => {
 export default PlayerList;
 
 const StyledList = styled.ul``;
-const StyledListItem = styled.li`
+const StyledListItem = styled.li<{ emphasize: boolean }>`
   height: 24px;
   display: flex;
   flex-direction: row;
   justify-content: left;
   align-items: center;
+  ${(props) => {
+    if (props.emphasize) {
+      return `color: white;`;
+    }
+    return '';
+  }}
 `;
 const StyledGird = styled.div<{ color: string }>`
-  width: 8px;
-  height: 8px;
+  width: 12px;
+  height: 12px;
   margin: 0 8px;
   position: relative;
   box-sizing: border-box;
@@ -55,8 +61,8 @@ const StyledGird = styled.div<{ color: string }>`
     left: -2px;
     display: block;
     content: '';
-    width: 8px;
-    height: 8px;
+    width: 12px;
+    height: 12px;
     box-sizing: border-box;
     border: solid 1px ${cssVars.colorText};
   }
